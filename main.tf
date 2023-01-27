fe1e381f91614dd76118d00b0c3b245cb492b1a3
leodama <leonardo@nitrotecnologia.com.br>
2023-01-26 23:35:32 -0300

    Commit inicial

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+
+resource "aws_instance" "app_server01" {
+  ami           = "ami-00874d747dde814fa"
+  instance_type = "t2.micro"
+  key_name = "terra-lab"
+
+  tags = {
+    Name = "AppServerInstance-lab"
+  }
+}
